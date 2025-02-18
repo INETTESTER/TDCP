99325e1250b4a61fb9952442fe4063054f7eac73
Tester Inet Doc <tester.inet.doc@gmail.com>
2025-02-18 11:45:11 +0700

    t

--- main/main.js
+++ main/main.js
@@ -31,8 +31,8 @@ export default function () {    //เรียกใช้ API ใน export def
 //=======================SCB==================================
   // response = oaut_scb()
   // response = callback_scb(scenario)
-   response = oauth_create_scb(cid)
-  // response = oauth_create_qr_scb()
+  // response = oauth_create_scb(cid)
+   response = oauth_create_qr_scb()
   // response = oauth_create_inquirypertxn_scb()
   // response = oauth_create_inquirydate_scb()
   // response = oauth_create_qr_callback_scb(scenario)
--- open.sh
+++ open.sh
@@ -4,10 +4,10 @@
 ##########################################################################
 ##########################################################################
 ##########################################################################
-                     projectname="oauth_create_scb" #ตั้งชื่อ projhttpsect ให้เหมือนกัน
+                     projectname="oauth_create_qr_scb" #ตั้งชื่อ projhttpsect ให้เหมือนกัน
                      google_sheet="https://docs.google.com/spreadsheets/d/13ZqmW82FnQj_uY-9VjUCPNvBMFEZJcodK1AmRn_euW0/edit?gid=180143788#gid=180143788" 
-                     id="34"                 #เปลี่ยน id ทุกครั้งที่ยิง
-                     user="10000";            #จำนวนผู้ใช้งาน
+                     id="1"                 #เปลี่ยน id ทุกครั้งที่ยิง
+                     user="5000";            #จำนวนผู้ใช้งาน
                      durationx="60";         #วินาที
                      scenario="3"           #scenario="1" ยิงเเบบกำหนด request (duration ได้แค่ 1 วินาที)
                      cid="112"                 #scenario="2" ยิงเเบบกำหนด VUs  (กำหนดว่า user x คน ใช้ระบบ x วินาที)
